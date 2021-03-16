ded9a6ce5761f4fd42d9cc05668bcc68e29eaed7
Truman <Truman.Chu@Honeywell.com>
2021-03-16 09:57:01 +0800

    truman update part 2 20210316

--- app.py
+++ app.py
@@ -4,8 +4,25 @@
 # @File : app.py
 
 from flask import Flask
+from flask import url_for
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def hello():
-    return "welcome to my notelist"
+    return '<h1>hello Truman!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User : %s' % name
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name = 'truman'))
+    print(url_for('user_page', name = 'peter'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num = 2))
+    return  'Test page'
