86191ac564c8e28d3359baed3b38e6485fff4765
Truman <Truman.Chu@Honeywell.com>
2021-03-18 15:59:39 +0800

    update SQLite for project 20210318

--- app.py
+++ app.py
@@ -6,9 +6,9 @@
 import os
 import sys
 
-from flask import Flask, render_template
-from flask import url_for
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+import click
 
 # SQLite URI compatible
 WIN = sys.platform.startswith('win')
@@ -20,8 +20,7 @@ else:
 # 实例初始化
 app = Flask(__name__)
 # 配置数据连接地址
-# app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 # 关闭对模型修改的监控
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -30,31 +29,30 @@ db = SQLAlchemy(app)
 
 #表名将会是user，自动生成，小写处理
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)    #主键
-    name = db.Column(db.String(20))                 #名字
+    id = db.Column(db.Integer, primary_key=True)    # 主键
+    name = db.Column(db.String(20))                 # 名字
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tile = db.Column(db.String(60))
-    notes = db.Column(db.String(20))
+    title = db.Column(db.String(60))
+    note = db.Column(db.String(20))
 
-name = 'Truman'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')
+    # 设置选项
+def initdb(drop):
+    """Initialize the database"""
+    if drop:
+        db.drop_all()
+        click.echo('drop the database. ')  # 输出提示信息
+    db.create_all()
+    click.echo('Initialized database. ')    # 输出提示信息
 
 @app.route('/')
 def index():
-    return render_template('index.html', name = name, movies = movies)
+    user = User.query.first()
+    notes = Notes.query.all()
+    return render_template('index.html', name = user.name, movies = notes)
 
 @app.route('/home')
 @app.route('/index')
@@ -72,4 +70,4 @@ def test_url_for():
     print(url_for('user_page', name = 'peter'))
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num = 2))
-    return  'Test page'
+    return 'Test page'
