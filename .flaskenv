3fb2f86d0c1741113a7cb917bcb59ef2bb3cf69e
Truman <Truman.Chu@Honeywell.com>
2021-04-02 13:27:04 +0800

    update env 20210402

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
-FLASK_ENV=development
 FLASK_APP=notesList
+FLASK_ENV=development
+FLASK_RUN_PORT=6876
--- test_noteslist.py
+++ test_noteslist.py
@@ -8,6 +8,26 @@ from notesList import app, db
 from notesList.models import User, Notes
 
 
+"""
+# for flaskenv config file
+# 
+# #默认为app.py
+# FLASK_APP=main
+# 
+# #默认为5000
+# FLASK_RUN_PORT=6666
+# 
+# #默认为localhost
+# FLASK_RUN_HOST=0.0.0.0
+# 
+# #development为开发环境，production为生产环境
+# FLASK_ENV=development
+# 
+# #1为开启，0为关闭
+# FLASK_DEBUG=1
+"""
+
+
 class NotesListTestCase(unittest.TestCase):
 
     def setUp(self):
